cf0583072ef7ab6a72840dca98bf46f9c85a40be
ribeirodavi04 <ribeirodavi04@outlook.com>
2022-05-02 20:22:06 -0300

    API fornecedores concluida

--- routes/fornecedores.js
+++ routes/fornecedores.js
@@ -4,29 +4,37 @@ const mysql = require("../mysql").pool;
 
 //RETORNA TODOS FORNECEDORES
 router.get("/", (req, res, next) => {
-  res.status(200).send({
-    message: "Retorna Fornecedores",
+  mysql.getConnection((error, conn) => {
+    if (error) {
+      return res.status(500).send({ error: error });
+    }
+    conn.query("SELECT * FROM fornecedores;", (error, resultado, fields) => {
+      if (error) {
+        return res.status(500).send({ error: error });
+      }
+      return res.status(200).send({ response: resultado });
+    });
   });
 });
 
 //INSERE UM FORNECEDOR
 router.post("/", (req, res, next) => {
   mysql.getConnection((error, conn) => {
+    if (error) {
+      return res.status(500).send({ error: error });
+    }
     conn.query(
-      "INSERT INTO fornecedor (nomeFornecedor, CNPJ_Fornecedor, telFornecedor) VALUES(?,?,?);",
+      "INSERT INTO fornecedores (nomeFornecedor, CNPJ_Fornecedor, telFornecedor) VALUES(?,?,?);",
       [req.body.nome, req.body.cnpj, req.body.telefone],
       (error, resultado, field) => {
         conn.release();
         if (error) {
-          return res.status(500).send({
-            error: error,
-            response: null,
-          });
+          return res.status(500).send({ error: error });
         }
 
         res.status(201).send({
-          message: "Produto inserido com sucesso",
-          id_produto: resultado.insertId,
+          message: "Forneceor inserido com sucesso",
+          idFornecedor: resultado.insertId,
         });
       }
     );
@@ -35,24 +43,70 @@ router.post("/", (req, res, next) => {
 
 //RETORNA OS DADOS DE UM FORNECEEDOR ESPECIFICO
 router.get("/:idFornecedor", (req, res, next) => {
-  const id = req.params.idFornecedor;
-  res.status(200).send({
-    message: "Detalhes de um fornecedor",
-    id: id,
+  mysql.getConnection((error, conn) => {
+    if (error) {
+      return res.status(500).send({ error: error });
+    }
+    conn.query(
+      "SELECT * FROM fornecedores WHERE idFornecedor = ?;",
+      [req.params.idFornecedor],
+      (error, resultado, fields) => {
+        if (error) {
+          return res.status(500).send({ error: error });
+        }
+        return res.status(200).send({ response: resultado });
+      }
+    );
   });
 });
 
 //ALTERA UM FORNECEDOR
 router.patch("/", (req, res, next) => {
-  res.status(201).send({
-    message: "0 fornecedor alterado",
+  mysql.getConnection((error, conn) => {
+    if (error) {
+      return res.status(500).send({ error: error });
+    }
+    conn.query(
+      `UPDATE fornecedores 
+            SET  nomeFornecedor = ?, 
+                CNPJ_Fornecedor = ?, 
+                telFornecedor = ?
+            WHERE idFornecedor = ?`,
+      [req.body.nome, req.body.cnpj, req.body.telefone, req.body.idFornecedor],
+      (error, resultado, field) => {
+        conn.release();
+        if (error) {
+          return res.status(500).send({ error: error });
+        }
+
+        res.status(202).send({
+          message: "Fornecedores alterado com sucesso",
+        });
+      }
+    );
   });
 });
 
 //DELETA UM FORNECEDOR
 router.delete("/", (req, res, next) => {
-  res.status(201).send({
-    message: "forncedor deletado",
+  mysql.getConnection((error, conn) => {
+    if (error) {
+      return res.status(500).send({ error: error });
+    }
+    conn.query(
+      "DELETE FROM fornecedores WHERE fornecedores.idFornecedor = ?",
+      [req.body.idFornecedor],
+      (error, resultado, field) => {
+        conn.release();
+        if (error) {
+          return res.status(500).send({ error: error });
+        }
+
+        res.status(202).send({
+          message: "Fornecedor removido com sucesso",
+        });
+      }
+    );
   });
 });
 
