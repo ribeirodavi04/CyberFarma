5e3f507d52ad2d0354e279e8f7f3d872dacaedaa
ribeirodavi04 <ribeirodavi04@outlook.com>
2022-05-13 00:12:19 -0300

    Login funcionando

--- cyberfarma-frontend/login.js
+++ cyberfarma-frontend/login.js
@@ -1,29 +1,32 @@
 function login() {
-    event.preventDefault();
-    let url = "http://localhost:3000/login/funcionario";
-  
-    let nomeUsuario = document.getElementsByName("nomeUsuario")[0].value;
-    let senha = document.getElementsByName("senha")[0].value;
-    
-  
-    console.log(nomeUsuario, senha);
-  
-    body = {
-      nomeUsuario: nomeUsuario,
-      senha: senha,
-    };
-  
-    fetch(url, {
-      method: "POST",
-      body: JSON.stringify(body),
-      headers: { "Content-type": "application/json; charset=UTF-8" },
+  event.preventDefault();
+  let url = "http://localhost:3000/login/funcionario";
+
+  let nomeUsuario = document.getElementsByName("nomeUsuario")[0].value;
+  let senha = document.getElementsByName("senha")[0].value;
+
+  console.log(nomeUsuario, senha);
+
+  body = {
+    nomeUsuario: nomeUsuario,
+    senha: senha,
+  };
+
+  fetch(url, {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers: { "Content-type": "application/json; charset=UTF-8" },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.message === "Falha na autenticação!") {
+        console.log(data.message);
+      } else {
+        window.location.href = "TelaDeRelatorios.html";
+      }
     })
-      .then((response) => response.json())
-      .then((json) => console.log(json)) 
-      .catch((err) => console.log(err));
+    .catch((err) => console.log(err));
 
-
-    window.location.href = "TelaDeRelatorios.html";
-    document.getElementsByName("nomeUsuario")[0].value = "";
-    document.getElementsByName("senha")[0].value = "";
-  }
+  document.getElementsByName("nomeUsuario")[0].value = "";
+  document.getElementsByName("senha")[0].value = "";
+}
